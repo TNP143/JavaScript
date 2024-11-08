53c0fa079ae4c7f8c853bad54be3d6ce97d94707
8282 <ant.thanapa@gmail.com>
2024-11-08 15:25:10 +0700

    Update index.js

--- 1.beginner/index.js
+++ 1.beginner/index.js
@@ -1,1 +1,8 @@
+//console.log('Hello');
+//console.log('I like pizza');
 
+//window.alert('This is alert');
+//window.alert('I like pizza');
+
+document.getElementById("H1").textContent = "Hello";
+document.getElementById("P").textContent = "I like tree";
